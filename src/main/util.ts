99f4f2b4567f7457006614d727568ae3a5b1a717
dctxf <dctmz@qq.com>
2023-11-20 13:38:26 +0800

    fix: version

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,12 +1,9 @@
-import { net } from 'electron';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import { URL } from 'url';
-import * as linux from './source/linux';
-import * as macos from './source/macos';
-import * as windows from './source/windows';
 
+import { exec } from 'child_process';
 import { randomUUID } from 'node:crypto';
 
 export function resolveHtmlPath(htmlFileName: string) {
@@ -103,44 +100,52 @@ export const getPicturePath = (options = {}) => {
 };
 
 // 设置为壁纸
-export const setWallpaperWith = async (input: string) => {
-  // 判断平台
-  let wallpaper = null;
-  if (process.platform === 'darwin') {
-    wallpaper = macos;
-  } else if (process.platform === 'win32') {
-    wallpaper = windows;
-  } else {
-    wallpaper = linux;
-  }
+// export const setWallpaperWith = async (input: string) => {
+//   // 判断平台
+//   let wallpaper = null;
+//   if (process.platform === 'darwin') {
+//     wallpaper = macos;
+//   } else if (process.platform === 'win32') {
+//     wallpaper = windows;
+//   } else {
+//     wallpaper = linux;
+//   }
 
-  const { setWallpaper } = wallpaper;
-  if (isUrl(input)) {
-    // 缓存远程图片到本地
-    // 获取input后缀
-    const extension = path.extname(input);
-    // 本地缓存目录 系统的缓存目录
-    // 缓存到系统的图片文件夹
-    const file = getPicturePath({ extension });
-    // 文件夹不存在则新建
-    if (!fs.existsSync(path.dirname(file))) {
-      fs.mkdirSync(path.dirname(file), { recursive: true });
-    }
-    // 获取远程图片
-    const response = await net.fetch(input);
-    if (response.ok && response.body) {
-      // 将chunk转换成stream
-      const reader = await response.body.getReader();
-      // reader to string
-      const { value } = await reader.read();
-      const buffer = Buffer.from(value!);
-      // 写入文件
-      fs.writeFileSync(file, buffer);
-      await setWallpaper(file);
-    }
-  } else if (isRGBHex(input)) {
-    // await setSolidColorWallpaper(input);
-  } else {
-    await setWallpaper(input);
-  }
+//   const { setWallpaper } = wallpaper;
+//   if (isUrl(input)) {
+//     // 缓存远程图片到本地
+//     // 获取input后缀
+//     const extension = path.extname(input);
+//     // 本地缓存目录 系统的缓存目录
+//     // 缓存到系统的图片文件夹
+//     const file = getPicturePath({ extension });
+//     // 文件夹不存在则新建
+//     if (!fs.existsSync(path.dirname(file))) {
+//       fs.mkdirSync(path.dirname(file), { recursive: true });
+//     }
+//     // 获取远程图片
+//     const response = await net.fetch(input);
+//     if (response.ok && response.body) {
+//       // 将chunk转换成stream
+//       const reader = await response.body.getReader();
+//       // reader to string
+//       const { value } = await reader.read();
+//       const buffer = Buffer.from(value!);
+//       // 写入文件
+//       fs.writeFileSync(file, buffer);
+//       await setWallpaper(file);
+//     }
+//   } else if (isRGBHex(input)) {
+//     // await setSolidColorWallpaper(input);
+//   } else {
+//     await setWallpaper(input);
+//   }
+// };
+
+export const setWallpaperWith = async (input: string) => {
+  const wallpaperBin = path.join(
+    __dirname,
+    '../../node_modules/.bin/wallpaper'
+  );
+  exec(`${wallpaperBin} ${input}`);
 };
